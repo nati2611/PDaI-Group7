760bf7e8ed614d26f61a78edc180ee59ccfc63fb
m-icha-l <mkowalsk24@students.oamk.fi>
2024-11-18 22:06:53 +0100

    Update sender_rp2.py

--- Rasberry/sender_rp2.py
+++ Rasberry/sender_rp2.py
@@ -1,22 +1,69 @@
 import RPi.GPIO as GPIO
-from time import sleep
+import time
+import threading
 
-pin = 14  
+wait = 1  # czas trwania stanu HIGH i LOW w sekundach
+clock_pin = 15
+sending_pin = 14
+flag = 1
+clock_state = False
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(clock_pin, GPIO.OUT)
+GPIO.setup(sending_pin, GPIO.OUT)
 
-GPIO.setup(pin, GPIO.OUT)  
+clock_time = time.time()  
+
+def clock():
+    global clock_time, flag, clock_state
+    
+    while True:
+        current_time = time.time()
+        
+       
+        if current_time - clock_time >= wait:
+            if flag % 2:  
+                GPIO.output(clock_pin, GPIO.HIGH)
+                clock_state = True
+
+            else:  
+                GPIO.output(clock_pin, GPIO.LOW)
+                clock_state = False
+
+            
+            clock_time = current_time
+            
+            if flag == 100000:
+                flag = 1
+            else:
+                flag += 1  
+
+        time.sleep(0.1)  
+
+clock_thread = threading.Thread(target=clock)
+clock_thread.daemon = True  
+clock_thread.start()
 
 try:
     while True:
-        GPIO.output(pin, GPIO.HIGH)  
-        print("HIGH")
-        sleep(2)
-        GPIO.output(pin, GPIO.LOW)   
-        print("LOW")
-        sleep(2)
+        if clock_state == True:
+            GPIO.output(sending_pin, GPIO.HIGH)
+            print("1")
+            time.sleep(wait)
+            GPIO.output(sending_pin, GPIO.HIGH)
+            print("2")
+            time.sleep(wait)
+            GPIO.output(sending_pin, GPIO.LOW)
+            print("3")
+            time.sleep(wait)
+            GPIO.output(sending_pin, GPIO.HIGH)
+            print("4")
+            time.sleep(wait)
+        
+
+        time.sleep(0.1)  
 
 except KeyboardInterrupt:
-    print("Przerwano przez użytkownika")
+    print("^ KUNIEC")
 
 finally:
-    GPIO.cleanup()  
+    GPIO.cleanup()
