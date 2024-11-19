e4a208d26b6865126daa20e347061dfff34342db
m-icha-l <mkowalsk24@students.oamk.fi>
2024-11-19 14:42:09 +0100

    Update sender_rp2.py

--- Rasberry/sender_rp2.py
+++ Rasberry/sender_rp2.py
@@ -2,50 +2,42 @@ import RPi.GPIO as GPIO
 import time
 import threading
 
-wait = 1  # czas trwania stanu HIGH i LOW w sekundach
+wait = 1
 clock_pin = 15
-sending_pin = 14
+reciving_pin = 14
+sending_pin = 18
 flag = 1
 clock_state = False
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(clock_pin, GPIO.OUT)
 GPIO.setup(sending_pin, GPIO.OUT)
+GPIO.setup(reciving_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(reciving_pin, GPIO.BOTH)
 
-clock_time = time.time()  
+clock_time = time.time()
 
 def clock():
     global clock_time, flag, clock_state
-    
     while True:
         current_time = time.time()
-        
-       
         if current_time - clock_time >= wait:
-            if flag % 2:  
+            if flag % 2:
                 GPIO.output(clock_pin, GPIO.HIGH)
                 clock_state = True
-
-            else:  
+            else:
                 GPIO.output(clock_pin, GPIO.LOW)
                 clock_state = False
-
-            
             clock_time = current_time
-            
-            if flag == 100000:
-                flag = 1
-            else:
-                flag += 1  
-
-        time.sleep(0.1)  
+            flag += 1
+        time.sleep(0.01)
 
 clock_thread = threading.Thread(target=clock)
-clock_thread.daemon = True  
+clock_thread.daemon = True
 clock_thread.start()
 
 try:
     while True:
-        if clock_state == True:
+        if clock_state:  # Synchronizacja z zegarem
             GPIO.output(sending_pin, GPIO.HIGH)
             print("1")
             time.sleep(wait)
@@ -58,9 +50,22 @@ try:
             GPIO.output(sending_pin, GPIO.HIGH)
             print("4")
             time.sleep(wait)
-        
-
-        time.sleep(0.1)  
+            break
+    while True:
+        if GPIO.event_detected(clock_pin):
+            if GPIO.input(reciving_pin) == GPIO.HIGH:
+                print("1")
+                time.sleep(wait)  
+                if GPIO.input(reciving_pin) == GPIO.HIGH:
+                    print("2")
+                    time.sleep(wait)
+                    if GPIO.input(reciving_pin) == GPIO.LOW:
+                        print("3")
+                        time.sleep(wait)
+                        if GPIO.input(reciving_pin) == GPIO.HIGH:
+                            print("4")
+                            print("connect")
+                            break
 
 except KeyboardInterrupt:
     print("^ KUNIEC")
