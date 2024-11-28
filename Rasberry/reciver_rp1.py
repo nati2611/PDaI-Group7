6ede1ef395a195a65a6564b741e9962c332164ef
m-icha-l <187118422+m-icha-l@users.noreply.github.com>
2024-11-28 02:14:05 +0100

    Update reciver_rp1.py
    Removed the clock from the equation. The Python libraries proved to work differently on different devices (small differences in responses from the time library; on a single device, the library works perfectly, but when comparing it with the one implemented on a second device, I observed minor, unilinear differences). This made syncing two independent clocks unnecessarily complicated for the scope of the project. I concluded that if I were to write my own clock based on the time library, I might as well trust the accuracy of the time.sleep() function. The solution is not ideal, and frankly, it's disappointingly simple, but as mentioned before, there is no point in creating a clock structure if I want to use Python and its libraries. If in the future I decide that implementing a clock is necessary (probably when trying to speed up data transfer), I will rewrite the code in C or C++.

--- Rasberry/reciver_rp1.py
+++ Rasberry/reciver_rp1.py
@@ -1,89 +1,53 @@
 import RPi.GPIO as GPIO
 import time
-import threading
 
-wait = 0.009
+wait = 0.01
 reciving_pin = 14
 sending_pin = 18
-clock_pin = 15
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(reciving_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(clock_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(sending_pin, GPIO.OUT)
-GPIO.add_event_detect(clock_pin, GPIO.BOTH)
+GPIO.add_event_detect(reciving_pin, GPIO.BOTH)
 
-'''___________synchro___________'''
-
-clock_end = False
-flag = 1
-clock_show_pin = 23
-GPIO.setup(clock_show_pin, GPIO.OUT)
-
-clock_time = time.perf_counter()  # Use high-precision timer
-
-def clock():
-    global clock_time, flag, clock_state, clock_end
-    while not clock_end:
-        current_time = time.perf_counter()  # Use high-precision timer
-        if current_time - clock_time >= wait:
-            if flag % 2:
-                GPIO.output(clock_show_pin, GPIO.HIGH)
-                clock_state = True
-            else:
-                GPIO.output(clock_show_pin, GPIO.LOW)
-                clock_state = False
-            clock_time = current_time
-            flag += 1
-        time.sleep(0.001)  # Minimal delay to prevent excessive CPU usage
-
-clock_thread = threading.Thread(target=clock)
-clock_thread.daemon = True
-clock_thread.start()
-
-'''___________synchro___________'''
+loop = True
 
 try:
-    while True:
-        if GPIO.event_detected(clock_pin):
+    while loop == True:
+        if GPIO.event_detected(reciving_pin):
             if GPIO.input(reciving_pin) == GPIO.HIGH:
                 print("1 - in")
-                while not GPIO.event_detected(clock_pin):
-                    pass  
+                time.sleep(wait)  
                 if GPIO.input(reciving_pin) == GPIO.HIGH:
                     print("2 - in")
-                    while not GPIO.event_detected(clock_pin):
-                        pass
+                    time.sleep(wait)
                     if GPIO.input(reciving_pin) == GPIO.LOW:
                         print("3 - in")
-                        while not GPIO.event_detected(clock_pin):
-                            pass
+                        time.sleep(wait)
                         if GPIO.input(reciving_pin) == GPIO.HIGH:
                             print("4 - in")
                             print("connect")
+                            loop = False
                             break
+                            
+    time.sleep(wait)
     while True:
-        if GPIO.event_detected(clock_pin):  # Synchronization with the clock
-            GPIO.output(sending_pin, GPIO.HIGH)
-            print("1 - out")
-            time.sleep(wait)
-            GPIO.output(sending_pin, GPIO.HIGH)
-            print("2 - out")
-            time.sleep(wait)
-            GPIO.output(sending_pin, GPIO.LOW)
-            print("3 - out")
-            time.sleep(wait)
-            GPIO.output(sending_pin, GPIO.HIGH)
-            print("4 - out")
-            time.sleep(wait)
-            '''_____________clock thread end_____________'''
-
-            clock_end = True 
-
-            '''_____________clock thread end_____________'''
-            break
+        GPIO.output(sending_pin, GPIO.HIGH)
+        print("1 - out")
+        time.sleep(wait)
+        GPIO.output(sending_pin, GPIO.HIGH)
+        print("2 - out")
+        time.sleep(wait)
+        GPIO.output(sending_pin, GPIO.LOW)
+        print("3 - out")
+        time.sleep(wait)
+        GPIO.output(sending_pin, GPIO.HIGH)
+        print("4 - out")
+        time.sleep(wait)
+        break
 
 except KeyboardInterrupt:
-    print("Interrupted by user")
+    print("^ end")
 
 finally:
     GPIO.cleanup()
