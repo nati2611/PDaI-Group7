a84f21e48a405835ce640e69645f09c537898b9a
m-icha-l <187118422+m-icha-l@users.noreply.github.com>
2024-12-02 22:40:46 +0100

    Update reciver_rp1.py to prep it to merge with hufman

--- Rasberry/reciver_rp1.py
+++ Rasberry/reciver_rp1.py
@@ -1,50 +1,75 @@
 import RPi.GPIO as GPIO
 import time
 
-wait = 0.01
+wait = 0.006
 reciving_pin = 14
-sending_pin = 18
+sending_pin = 15
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(reciving_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(sending_pin, GPIO.OUT)
 GPIO.add_event_detect(reciving_pin, GPIO.BOTH)
 
-loop = True
 
+
+recived= ""
 try:
-    while loop == True:
+    while True:
         if GPIO.event_detected(reciving_pin):
             if GPIO.input(reciving_pin) == GPIO.HIGH:
-                print("1 - in")
                 time.sleep(wait)  
                 if GPIO.input(reciving_pin) == GPIO.HIGH:
-                    print("2 - in")
                     time.sleep(wait)
                     if GPIO.input(reciving_pin) == GPIO.LOW:
-                        print("3 - in")
                         time.sleep(wait)
                         if GPIO.input(reciving_pin) == GPIO.HIGH:
-                            print("4 - in")
                             print("connect")
-                            loop = False
                             break
                             
     time.sleep(wait)
     while True:
         GPIO.output(sending_pin, GPIO.HIGH)
-        print("1 - out")
         time.sleep(wait)
         GPIO.output(sending_pin, GPIO.HIGH)
-        print("2 - out")
         time.sleep(wait)
         GPIO.output(sending_pin, GPIO.LOW)
-        print("3 - out")
         time.sleep(wait)
         GPIO.output(sending_pin, GPIO.HIGH)
-        print("4 - out")
         time.sleep(wait)
         break
+    GPIO.output(sending_pin, GPIO.LOW)
+    time.sleep(0.1)
+    if GPIO.event_detected(reciving_pin):
+        print("reciving...")
+    flag = False
+    while True:
+        if GPIO.event_detected(reciving_pin):
+            if GPIO.input(reciving_pin) == GPIO.HIGH:
+                time.sleep(wait)  
+                if GPIO.input(reciving_pin) == GPIO.HIGH:
+                    time.sleep(wait)
+                    if GPIO.input(reciving_pin) == GPIO.LOW:
+                        time.sleep(wait)
+                        if GPIO.input(reciving_pin) == GPIO.HIGH:
+                            time.sleep(wait)
+                            while True:
+                                if GPIO.input(reciving_pin) == GPIO.HIGH:
+                                    recived = recived + "1"
+                                    print("recived..",recived )
+                                    time.sleep(wait)     
+                                else:
+                                    recived = recived + "0"
+                                    print("recived..", recived)
+                                    time.sleep(wait)
+                                if recived.endswith("1101") and len(recived) > 4:
+                                    flag = True
+                                    break
+        if flag == True:
+            recived = recived[0:-4]
+            print(recived)
+            recived = ""
+            flag = False
+            GPIO.event_detected(reciving_pin)
 
 except KeyboardInterrupt:
     print("^ end")
